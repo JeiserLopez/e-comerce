4b350e1029a73b411e737ad15a917f4b75485b1c
Jeiser Lopez <jeiserlopez@hotmail.com>
2022-05-06 16:23:35 -0500

    datos json

--- js/app.js
+++ js/app.js
@@ -7,3 +7,21 @@ inputs.forEach((input) => {
     valida(input.target);
   });
 });
+
+var data;
+ 
+$(document).ready(function() {
+	$.ajax({
+		url: "../db.json",
+		data: "nocache=" + Math.random(),
+		type: "GET",
+		dataType: "json",
+		success: function(source){
+			data = source;
+			//mostrarJSON();
+		},
+		error: function(data){
+			alert("ERROR");
+		}
+	});							
+});
